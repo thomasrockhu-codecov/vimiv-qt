d22488b8e17815b9f4b47b147ad5c9f230c8ad06
karlch <karlch@protonmail.com>
2021-12-16 15:58:04 +0100

    Avoid lazy loading

--- tests/unit/test_debug.py
+++ tests/unit/test_debug.py
@@ -1,9 +1,8 @@
 # vim: ft=python fileencoding=utf-8 sw=4 et sts=4
 
-from vimiv.utils import lazy
-
 
 def test_dummy():
-    pyexiv2 = lazy.import_module("pyexiv2", optional=True)
+    import pyexiv2
+
     metadata_cls = pyexiv2.ImageMetadata
     assert metadata_cls is not None
